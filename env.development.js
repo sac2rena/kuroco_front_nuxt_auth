8150088514963bb05368fb10df3aa2acbb4e85ae
rena <rena@sacsac.co.jp>
2023-06-21 20:08:31 +0900

    envファイル追加

--- env.development.js
+++ env.development.js
@@ -1,5 +1,5 @@
 module.exports = {
     META_TITLE: '[DEV]Nuxt Auth',
     ROBOTS: 'noindex',
-    BASE_URL: 'https://dev-nuxt-auth.a.kuroco.app'
+    BASE_URL: 'https://dev-sac2-taka.g.kuroco-front.app/'
 };
